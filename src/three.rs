9425dd5eecca2f981d3fa4e13acc230b0d41dca1
Michael Campbell <me@micycle.dev>
2025-12-06 11:54:53 -0500

    three pt2

--- src/three.rs
+++ src/three.rs
@@ -1,14 +1,23 @@
+#![allow(clippy::cast_possible_wrap)]
 use std::{fs::File, io::Read};
 
-fn day_one_rec(nums: &[u64], digits_left: u32, acc: u64) -> u64 {
+fn solution_rec(nums: &[u64], digits_left: u32, acc: u64) -> u64 {
     if digits_left == 0 {
         acc
+    } else if digits_left as usize == nums.len() {
+        solution_rec(&nums[1..], digits_left - 1, acc + nums[0] * 10u64.pow(digits_left - 1))
     } else {
+        let mut pairs = nums[0..=nums.len() - digits_left as usize].iter()
+            .enumerate()
+            .collect::<Vec<_>>();
+        pairs.sort_by_key(|&(_, &n)| -(n as i64));
+
         let mut ret = 0;
-        for (idx, n) in nums.iter().enumerate() {
-            let new_acc = acc + n * 10u64.pow(digits_left - 1);
-            ret = ret.max(day_one_rec(&nums[(idx + 1)..], digits_left - 1, new_acc));
+        let biggest_n = *pairs[0].1;
+        for (idx, &n) in pairs.into_iter().filter(|(_, n)| **n == biggest_n) {
+            ret = ret.max(solution_rec(&nums[idx + 1..], digits_left - 1, acc + n * 10u64.pow(digits_left - 1)));
         }
+
         ret
     }
 }
@@ -22,7 +31,7 @@ pub fn solution(f: &mut File, part_two: bool) {
     let mut ret = 0;
     for line in buf.lines() {
         let nums = line.chars().map(|c| c.to_string().parse::<u64>().unwrap()).collect::<Vec<_>>();
-        ret += day_one_rec(&nums, 2, 0);
+        ret += solution_rec(&nums, if part_two { 12 } else { 2 }, 0);
     }
 
     println!("{ret}");
