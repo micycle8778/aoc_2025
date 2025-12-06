1885f9c38b07659f72bc346c38348581cb1c6139
Michael Campbell <me@micycle.dev>
2025-12-06 12:05:21 -0500

    no more for loops in day three >:)

--- src/three.rs
+++ src/three.rs
@@ -12,13 +12,11 @@ fn solution_rec(nums: &[u64], digits_left: u32, acc: u64) -> u64 {
             .collect::<Vec<_>>();
         pairs.sort_by_key(|&(_, &n)| -(n as i64));
 
-        let mut ret = 0;
         let biggest_n = *pairs[0].1;
-        for (idx, &n) in pairs.into_iter().filter(|(_, n)| **n == biggest_n) {
-            ret = ret.max(solution_rec(&nums[idx + 1..], digits_left - 1, acc + n * 10u64.pow(digits_left - 1)));
-        }
-
-        ret
+        pairs.into_iter()
+            .filter(|(_, n)| **n == biggest_n)
+            .map(|(idx, &n)| solution_rec(&nums[idx + 1..], digits_left - 1, acc + n * 10u64.pow(digits_left - 1)))
+            .max().unwrap()
     }
 }
 
@@ -28,11 +26,10 @@ pub fn solution(f: &mut File, part_two: bool) {
     let mut buf = String::new();
     f.read_to_string(&mut buf).unwrap();
 
-    let mut ret = 0;
-    for line in buf.lines() {
-        let nums = line.chars().map(|c| c.to_string().parse::<u64>().unwrap()).collect::<Vec<_>>();
-        ret += solution_rec(&nums, if part_two { 12 } else { 2 }, 0);
-    }
-
-    println!("{ret}");
+    println!("{}",
+        buf.lines()
+            .map(|line| line.chars().map(|c| c.to_string().parse::<u64>().unwrap()).collect::<Box<[_]>>())
+            .map(|nums| solution_rec(&nums, if part_two { 12 } else { 2 }, 0))
+            .sum::<u64>()
+    );
 }
